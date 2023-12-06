7202f39e0a36ebb58860910afccb5a8bf7b0871c
gourranz <Zakaria.Gourrane@lions.enc.edu>
2023-12-06 01:11:02 -0500

    main2

--- main2.js
+++ main2.js
@@ -1,27 +1,40 @@
 
+const ball = document.getElementById('ball');
+const gameContainer = document.getElementById('container');
+const computedStyles = window.getComputedStyle(ball);
+const bricks = document.querySelectorAll('.brick');
+const scoreElement = document.getElementById('scores');
+const gameOverMessage = document.getElementById('game-over-message');
+const playAgainButton = document.getElementById('play-again-btn');
+let brickStates;
+let score = 0;
+let gameRunning = false;
+let x = 402; // Initial x position
+let y = 600; // Initial y position
+let speedX = 2 ;
+let speedY = -6;
+let brickHitCooldown = false;
+let backgroundMusic = document.getElementById('backgroundMusic');
 const paddle = document.getElementById('playerPaddle');
-const brickContainer = document.getElementById('brick');
-let brickPosition = 0; // Declare brickPosition here
-let direction = 5; // Adjust the speed as needed
 let movingLeft = false;
 let movingRight = false;
 let gameover = false;
-const brickSpeed = 2;
-const initialPaddlePosition = {
-bottom: parseFloat(getComputedStyle(paddle).bottom),
-left: parseFloat(getComputedStyle(paddle).left)
+const initialPaddlePosition = { // Intializing the Paddle position
+    bottom: parseFloat(getComputedStyle(paddle).bottom),
+    left: parseFloat(getComputedStyle(paddle).left)
 };
 
 
-//
+//Moving the Player Paddle
 document.addEventListener('keydown', function (event) {
+    if (gameRunning) {
     if (event.key === 'ArrowLeft') {
         movingLeft = true;
     } else if (event.key === 'ArrowRight') {
         movingRight = true;
     }
+}
 });
-
 document.addEventListener('keyup', function (event) {
     if (event.key === 'ArrowLeft') {
         movingLeft = false;
@@ -29,7 +42,7 @@ document.addEventListener('keyup', function (event) {
         movingRight = false;
     }
 });
-
+//Moving the Paddle right to left within the game Screen 
 function movePaddle() {
     const currentPosition = parseFloat(window.getComputedStyle(paddle).left);
 
@@ -42,57 +55,10 @@ function movePaddle() {
     }
 }
 
-setInterval(movePaddle,20)
-
-const ball = document.getElementById('ball');
-const gameContainer = document.getElementById('container');
-const computedStyles = window.getComputedStyle(ball);
-const bricks = document.querySelectorAll('.brick');
-const scoreElement = document.getElementById('scores');
-const gameOverMessage = document.getElementById('game-over-message');
-const playAgainButton = document.getElementById('play-again-btn');
-let brickStates;
-
-
-let score = 0;
-
-//Generating the bricks
-
-/*function generateBricks(rows, cols) {
-    const bricks = [];
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            const brick = document.createElement('div');
-            brick.className = 'brick';
-            gameContainer.appendChild(brick);
-            bricks.push(brick);
-        }
-    }
-
-    return bricks;
-}
-*/
-
-//const bricks = generateBricks(5, 5);
-
-        let gameRunning = false;
-        let x = 402; // Initial x position
-        let y = 600; // Initial y position
-        let speedX = 2 ;
-        let speedY = -4;
-        let brickHitCooldown = false;
-        let backgroundMusic = document.getElementById('backgroundMusic');
-        
-       /* function startBackgroundMusic() {
-            backgroundMusic.play();
-        }
-        
-        function stopBackgroundMusic() {
-            backgroundMusic.pause();
-            backgroundMusic.currentTime = 0;
-        } */
-  
-        function updateBallPosition() {
+setInterval(movePaddle, 20);
+ 
+        // update Ball position 
+function updateBallPosition() {
             
             if (!gameRunning) {
                 return;
@@ -103,27 +69,27 @@ let score = 0;
             x += speedX;
             y += speedY;
     
-            for (const brick of bricks) {
+            for (const brick of bricks) { // Detect collision on the bricks
                 if (!brickHitCooldown && !brick.classList.contains('break') && isCollision(ball, brick)) {
                     brick.classList.add('break') ; // Hide the brick
                     brickHitCooldown = true;
                     score+=10; 
-                    playSound('Sounds/Lego Break  Fall Apart - Sound Effect (HD).mp3');
+                    playSound('Sounds/Lego Break  Fall Apart - Sound Effect (HD).mp3'); // Play a sound for each brick that has been destroyed
+                    
                     scoreElement.textContent = `Score: ${score}`;// Set cooldown
                     setTimeout(() => {
                         brickHitCooldown = false;
-                    }, 200); // Adjust the delay as needed
-                    speedY = -speedY;
-                 // Reverse ball's vertical direction
+                    }, 200); 
+                    speedY = -speedY;// Reverse ball's vertical direction
                 }
-                if (!brick.classList.contains('break')) {
+                if (!brick.classList.contains('break')) { //Check of all the bricks have been destroyed 
                     allBricksBroken = false;
                 }
             }
 
             if (allBricksBroken) {
                 gameRunning = false; // Stop the game
-                displayWinningMessage();
+                displayWinningMessage(); // Display winning message if all the bricks destroyed
                 return;
             }
     
@@ -142,7 +108,6 @@ let score = 0;
             }
             if (y + ball.clientHeight > gameContainer.clientHeight) {
                 // Ball passed the paddle, game over
-                gameRunning = false;
                 gameOver();
                 return;
             }
@@ -151,8 +116,9 @@ let score = 0;
     
             ball.style.left = x + 'px';
             ball.style.top = y + 'px';
-        }
-        function isCollision(element1, element2) {
+}
+
+function isCollision(element1, element2) { // Collision Function
             const rect1 = element1.getBoundingClientRect();
             const rect2 = element2.getBoundingClientRect();
     
@@ -162,9 +128,9 @@ let score = 0;
                 rect1.y < rect2.y + rect2.height &&
                 rect1.y + rect1.height  > rect2.y
             );
-        }
+}
 
-        function handleSpaceKey(event) {
+function handleSpaceKey(event) { // Check if the space key been pressed and if so start the game
             if (event.code === 'Space') {
 
                 if (!gameRunning && !gameover) {
@@ -172,15 +138,15 @@ let score = 0;
                 
                     gameRunning = true;
                     // Move the ball upward when space key is pressed
-                    speedY = -4; // spead of the ball
+                    speedY = -6; // spead of the ball
                     gameLoop();
                 } else {
                     gameRunning = false;
                 }
             }
-            
-        }
-        function displayWinningMessage() {
+}
+
+function displayWinningMessage() {
             // Display your winning message or perform any actions
             // For example, you can show an element with an id 'win-message'
            
@@ -188,30 +154,30 @@ let score = 0;
             const buttonMessage = document.getElementById ('next-level')
             winMessageElement.style.display = 'block';
             buttonMessage.style.display = 'block'
-        }
-        
-        function gameOver() {
+}
+
+function gameOver() { 
              gameRunning = false;
-             gameover = true;
-             const brickContainer = document.getElementById("brick");
-             brickContainer.classList.add("paused");
-          
+            gameover = true;
             playSound('Sounds/mixkit-arcade-game-opener-222.wav');
             gameRunning = false;
             gameOverMessage.style.display = 'block';
             playAgainButton.style.display = 'block';
-        }
-        function playAgain() {
+}
+
+function playAgain() {
             gameOverMessage.style.display = 'none';
             playAgainButton.style.display = 'none';
             gameover = false;
             resetGame();
-        }
-        function playSound(soundFile) {
+}
+
+function playSound(soundFile) {
             const sound = new Audio(soundFile);
             sound.play();
-        }
-        function resetGame() {
+}
+
+function resetGame() {
             // Reset ball and other game elements
             x = 402; // Initial x position
             y = 600; // Initial y position
@@ -227,8 +193,8 @@ let score = 0;
         
             Array.from(document.querySelectorAll('.brick')).forEach((brick, index) => {
                 brick.classList.remove('break'); // Remove 'break' class
-        brickStates[index] = true; // Reset brick state
-        brick.style.display = 'block'; // Show the brick
+            brickStates[index] = true; // Reset brick state
+            brick.style.display = 'block'; // Show the brick
             });
             score=0; 
                     
@@ -237,21 +203,15 @@ let score = 0;
             // Start the game
             gameRunning = true;
             gameLoop();
-        }
+}
 
-        function gameLoop() {
+function gameLoop() {
            
             updateBallPosition();
             if (gameRunning) {
                 requestAnimationFrame(gameLoop);
             }
-        }
+}
     
-    
-
-
-        // Add event listener for the space key
-        document.addEventListener('keydown', handleSpaceKey);
-
-
-
+    // Add event listener for the space key
+document.addEventListener('keydown', handleSpaceKey);
